902805abca985e2589f63f21286d6fa07e54daf7
phatommobb <nickoluoch1130@gmail.com>
2024-12-21 14:43:13 +0300

    Project initialization

--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -3,12 +3,6 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'hero-desktop': "url('assets/images/bg-main-desktop.png')",
-        'hero-mobile': "url('assets/images/bg-main-mobile.png')",
-        'hero-card-front': "url('assets/images/bg-card-front.png')",
-        'hero-card-back': "url('assets/images/bg-card-back.png')",
-      },
       fontSize: {
         xxs: '0.65rem',
       },
