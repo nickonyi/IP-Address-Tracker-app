f8390fc33ed699bcfcd619085d19392f518c08fc
phatommobb <nickoluoch1130@gmail.com>
2024-12-27 20:21:41 +0300

    HTML and CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'hero-desktop': "url('assets/images/pattern-bg-desktop.png')",
+        'hero-mobile': "url('assets/images/pattern-bg-mobile.png')",
+      },
       fontSize: {
         xxs: '0.65rem',
+      },
+      colors: {
+        'very-dark-gray': 'hsl(0, 0%, 17%)',
+        'dark-gray': 'hsl(0, 0%, 59%)',
       },
     },
   },
