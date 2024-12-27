4a9ba6957bb42199b170715b54bc5030955488ff
phatommobb <nickoluoch1130@gmail.com>
2024-12-27 19:46:52 +0300

    HTML and CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/style.css';
 
-let map = L.map('map').setView([-1.4563, 36.97826], 13);
+let map = L.map('map').setView([-1.29524, 36.81233], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
@@ -8,9 +8,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-let marker = L.marker([-1.4563, 36.97826]).addTo(map);
+let marker = L.marker([-1.29524, 36.81233]).addTo(map);
 
-let circle = L.circle([-1.4563, 36.97826], {
+let circle = L.circle([-1.29524, 36.81233], {
   color: 'yellow',
   fillColor: '#f03',
   fillOpacity: 0.5,
