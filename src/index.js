effbd55cef8c1a4a7eb88c722a75c73e56e1d9cf
phatommobb <nickoluoch1130@gmail.com>
2025-01-07 18:54:48 +0300

    creating map using leaflet

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 import './styles/style.css';
+import { createMap } from './scripts/maps';
 
-let map = L.map('map').setView([-1.29524, 36.81233], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-
-let marker = L.marker([-1.29524, 36.81233]).addTo(map);
-
-let circle = L.circle([-1.29524, 36.81233], {
-  color: 'yellow',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 500,
-}).addTo(map);
+const map = createMap();
---
+++ src/scripts/maps.js
@@ -0,0 +1,27 @@
+export const createMap = () => {
+  let map = L.map('map').setView([-1.29524, 36.81233], 13);
+
+  const mapTile = L.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    },
+  ).addTo(map);
+
+  let marker = L.marker([-1.29524, 36.81233]).addTo(map);
+
+  let circle = L.circle([-1.29524, 36.81233], {
+    color: 'yellow',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500,
+  }).addTo(map);
+
+  return {
+    mapTile,
+    marker,
+    circle,
+  };
+};
