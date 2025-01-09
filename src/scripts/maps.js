70b6162dc82c2db7e738c1535165b56110045bc2
phatommobb <nickoluoch1130@gmail.com>
2025-01-09 21:10:20 +0300

    search functionality

--- src/scripts/maps.js
+++ src/scripts/maps.js
@@ -1,10 +1,15 @@
 import { fetchIPGeolocation } from './geolocator';
 
+let map = '';
+
 export const createMap = async (ip) => {
   const coordinatesData = await fetchIPGeolocation(ip);
   const coordinates = coordinatesData.location;
 
-  let map = L.map('map').setView([coordinates.lat, coordinates.lng], 14);
+  if (map) {
+    map.remove();
+  }
+  map = L.map('map').setView([coordinates.lat, coordinates.lng], 14);
 
   const mapTile = L.tileLayer(
     'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
